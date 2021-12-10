7389376f954c95e2af9e9bca8f301703f67ae426
Ubuntu <ubuntu@ip-172-31-29-83.eu-central-1.compute.internal>
2021-12-10 11:30:46 +0000

    Ders için düzenlemeler yapıldı

--- courses/faker/faker_demo.py
+++ courses/faker/faker_demo.py
@@ -1,8 +1,32 @@
+import psycopg2
+
 from faker import Faker
 fake = Faker()
 
-for _ in range(3):
-  print(fake.name())
-  print(fake.phone_number())
-  print(fake.address())
-  print("\n")
+try:
+    connection = psycopg2.connect(user="postgres",
+                                  password="smyo",
+                                  host="127.0.0.1",
+                                  port="5432",
+                                  database="smyo")
+    cursor = connection.cursor()
+
+    insert_query = """ INSERT INTO employees (department_id, name, salary, email, phone_number) VALUES (%s,%s,%s,%s,%s) """
+    count = 1
+    
+    for _ in range(3000):
+      record_to_insert = (fake.random_int(min=1, max=8), fake.name(), fake.random_int(min=5500, max=75600), fake.unique.email(), fake.unique.phone_number())
+      cursor.execute(insert_query, record_to_insert)
+      connection.commit()
+      print(count, "Record inserted successfully into employees table")
+      count +=  1
+
+except (Exception, psycopg2.Error) as error:
+    print("Failed to insert record into employees table", error)
+
+finally:
+    # closing database connection.
+    if connection:
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")
